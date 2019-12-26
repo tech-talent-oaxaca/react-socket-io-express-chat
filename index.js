d8220ed1cc0a7cce977faf018ac98e6fd1efe262
Edgar Talledos <onimenotsuki@gmail.com>
2019-12-25 22:11:22 -0600

    Terminado chat básico con éxito

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // Socket IO
 const http = require('http');
-const sockeIo = require('socket.io');
+const socketIo = require('socket.io');
 
 // Webpack
 const webpack = require('webpack');
@@ -12,10 +12,26 @@ const config = require('./webpack.config');
 
 const app = express();
 const server = http.createServer(app);
-const io = sockeIo(server);
+const io = socketIo(server);
 
 // Middleware
-app.use(webpackDevMiddleware(webpack(config)));
+app.use(
+  webpackDevMiddleware(webpack(config), {
+    noInfo: true,
+  }),
+);
 app.use(express.static(__dirname + '/public'));
+
+// Sockect connection
+io.on('connection', (socket) => {
+  console.log('socket connected: ', socket.id);
+
+  socket.on('message', (body) => {
+    socket.broadcast.emit('message', {
+      body,
+      from: socket.id.slice(4),
+    });
+  });
+});
 
 server.listen(3000, () => console.log('Server running on localhost:3000'));
--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+import io from 'socket.io-client';
 
-const App = () => (
-  <>
-    <h1>Integer eget aliquet nibh praesent.</h1>
-    <h2>Hola desde React</h2>
-  </>
-);
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      messages: [],
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    const body = event.target.value;
+    const code = event.keyCode || event.which;
+
+    if (code === 13 && body) {
+      const message = {
+        from: 'me',
+        body,
+      };
+
+      this.setState({ messages: [message, ...this.state.messages] });
+      this.socket.emit('message', body);
+      event.target.value = '';
+    }
+  }
+
+  componentDidMount() {
+    this.socket = io('/');
+    this.socket.on('message', (message) =>
+      this.setState({ messages: [message, ...this.state.messages] }),
+    );
+  }
+
+  render() {
+    const messages = this.state.messages.map((message, index) => (
+      <li key={index}>
+        <b>
+          {message.from}: {message.body}
+        </b>
+      </li>
+    ));
+
+    return (
+      <>
+        <h1>React Chat</h1>
+        <input
+          type="text"
+          placeholder="Insert a message"
+          onKeyPress={this.handleSubmit}
+        />
+        {messages}
+      </>
+    );
+  }
+}
 
 export default App;
